d984521ebbd9389319e82b67dbf39abab791ba8b
Dmytro Sopin <Dmytro_Sopin@epam.com>
2016-04-20 11:44:38 +0300

    pushed cpd-array-max module

---
+++ README.md
@@ -0,0 +1,9 @@
+Project for CDP task.
+
+Install:
+
+git clone https://github.com/dmSopin/cpd-array-max.git
+
+or
+
+npm i cpd-array-max
---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = (arr) => {
+    var max;
+
+    if(Array.isArray(arr) && arr.length) { //if arr is not Array or is epmty return null
+        max = arr[0];
+
+        for(var i = 0, length = arr.length; i < length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    } else  {
+        return null;
+    }
+
+}
